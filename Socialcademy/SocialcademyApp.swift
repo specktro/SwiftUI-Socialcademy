6ec67624326d587b2cce975d4f1260cc52cf5163
@specktro <specktro@nonull.mx>
2024-12-11 23:48:10 -0600

    Initial commit with gitignore file

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by specktro on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
