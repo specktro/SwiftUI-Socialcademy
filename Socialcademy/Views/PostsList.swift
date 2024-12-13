de4157fca127f1f6ded0fa53a8f4e6dc2e7211a7
@specktro <specktro@nonull.mx>
2024-12-13 09:18:37 -0600

    feat: Add search behavior to PostsList view

--- Socialcademy/Views/PostsList.swift
+++ Socialcademy/Views/PostsList.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct PostsList: View {
+    @State private var searchText: String = ""
     private var posts = [Post.testPost]
     
     var body: some View {
         NavigationView {
             List(posts) { post in
-                PostRow(post: post)
+                if searchText.isEmpty || post.contains(searchText) {
+                    PostRow(post: post)
+                }
             }
+            .searchable(text: $searchText)
             .navigationTitle("Posts")
         }
     }
