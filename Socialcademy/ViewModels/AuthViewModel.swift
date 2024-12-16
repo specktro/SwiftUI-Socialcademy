3b0b589a871fed082938519cdc6acd03431144c6
@specktro <specktro@nonull.mx>
2024-12-16 15:51:58 -0600

    feat: Add sign in and sign up forms with FirebaseAuth support

--- Socialcademy/ViewModels/AuthViewModel.swift
+++ Socialcademy/ViewModels/AuthViewModel.swift
@@ -10,8 +10,6 @@ import SwiftUI
 @MainActor
 final class AuthViewModel: ObservableObject {
     @Published var isAuthenticated: Bool = false
-    @Published var email = ""
-    @Published var password = ""
     
     private let authService = AuthService()
     
@@ -19,13 +17,26 @@ final class AuthViewModel: ObservableObject {
         authService.$isAuthenticated.assign(to: &$isAuthenticated)
     }
     
-    func signIn() {
-        Task {
-            do {
-                try await authService.signIn(email: email, password: password)
-            } catch {
-                print("[AuthViewModel] Cannot sign in: \(error)")
-            }
+    func makeSignInViewModel() -> SignInViewModel {
+        return SignInViewModel(action: authService.signIn(email:password:))
+    }
+
+    func makeCreateAccountViewModel() -> CreateAccountViewModel {
+        return CreateAccountViewModel(action: authService.createAccount(name:email:password:))
+    }
+}
+
+
+extension AuthViewModel {
+    class SignInViewModel: FormViewModel<(email: String, password: String)> {
+        convenience init (action: @escaping Action) {
+            self.init(initialValue: (email: "", password: ""), action: action)
+        }
+    }
+    
+    class CreateAccountViewModel: FormViewModel<(name: String, email: String, password: String)> {
+        convenience init (action: @escaping Action) {
+            self.init(initialValue: (name: "", email: "", password: ""), action: action)
         }
     }
 }
--- Socialcademy/Views/AuthView.swift
+++ Socialcademy/Views/AuthView.swift
@@ -14,13 +14,45 @@ struct AuthView: View {
         if viewModel.isAuthenticated {
             MainTabView()
         } else {
+            NavigationView {
+                SignInForm(viewModel: viewModel.makeSignInViewModel()) {
+                    NavigationLink(
+                        "Create Account",
+                        destination: CreateAccountForm(viewModel: viewModel.makeCreateAccountViewModel())
+                    )
+                }
+            }
+        }
+    }
+}
+
+private extension AuthView {
+    struct CreateAccountForm: View {
+        @StateObject var viewModel: AuthViewModel.CreateAccountViewModel
+        
+        var body: some View {
+            Form {
+                TextField("Name", text: $viewModel.name)
+                TextField("Email", text: $viewModel.email)
+                SecureField("Password", text: $viewModel.password)
+                Button("Create Account", action: viewModel.submit)
+            }
+            .navigationTitle("Create Account")
+        }
+    }
+    
+    struct SignInForm<Footer: View>: View {
+        @StateObject var viewModel: AuthViewModel.SignInViewModel
+        @ViewBuilder let footer: () -> Footer
+        
+        var body: some View {
             Form {
                 TextField("Email", text: $viewModel.email)
                 SecureField("Password", text: $viewModel.password)
-                Button("Sign In") {
-                    viewModel.signIn()
-                }
+                Button("Sign In", action: viewModel.submit)
+                footer()
             }
+            .navigationTitle("Sign In")
         }
     }
 }
